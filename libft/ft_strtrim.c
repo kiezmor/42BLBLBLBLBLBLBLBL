0345c2455e6a50a228ad20adce5b86e82bda6aac
Vincent PLUCHARD <vpluchar@e2r11p1.42.fr>
2016-11-23 23:27:54 +0100

    42

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/23 00:06:58 by vpluchar          #+#    #+#             */
-/*   Updated: 2016/11/23 23:21:34 by vpluchar         ###   ########.fr       */
+/*   Created: 2016/11/23 23:24:36 by vpluchar          #+#    #+#             */
+/*   Updated: 2016/11/23 23:25:11 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strtrim(const char *s)
 {
-	char	*tmp;
-	size_t	size;
-
-	if (!s1 || !s2)
-		return (NULL);
-	size = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
-	tmp = malloc(sizeof(char) * size);
-	if (tmp != NULL)
-		ft_strcat(ft_strcpy(tmp, s1), s2);
-	tmp[size] = '\0';
-	return (tmp);
+	
 }
