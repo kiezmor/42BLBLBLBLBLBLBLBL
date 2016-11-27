b1f2c283d151795087ad7888cc6736b936e4313a
Vincent PLUCHARD <vpluchar@e1r4p18.42.fr>
2016-11-27 22:21:39 +0100

    42

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 02:04:33 by vpluchar          #+#    #+#             */
-/*   Updated: 2016/11/27 19:52:37 by vpluchar         ###   ########.fr       */
+/*   Updated: 2016/11/27 21:53:26 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,6 @@ char	*ft_strstr(const char *b, const char *s)
 {
 	int j;
 
-<<<<<<< HEAD
-	v = 0;
-	p = 0;
-	if (l[0] == '\0')
-		return ((char *)b);
-	while (b[p] != '\0')
-	{
-		while (b[p + v] == l[v])
-		{
-			v++;
-			if (l[v] == '\0')
-				return ((char *)b);
-		}
-		p++;
-		v = 0;
-=======
 	j = 0;
 	if (!(*s))
 		return ((char *)b);
@@ -43,7 +27,6 @@ char	*ft_strstr(const char *b, const char *s)
 			return ((char *)b);
 		j = 0;
 		b++;
->>>>>>> ee8f5868f4273166751623ae067974a646d739f0
 	}
 	return (NULL);
 }
