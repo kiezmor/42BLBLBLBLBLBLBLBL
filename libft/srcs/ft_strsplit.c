f311dd317aa436fb09b4f4eae5b31b6b04a31898
Vincent PLUCHARD <vpluchar@e2r11p1.42.fr>
2016-11-29 00:08:20 +0100

    42

--- libft/srcs/ft_strsplit.c
+++ libft/srcs/ft_strsplit.c
@@ -6,37 +6,48 @@
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 00:24:07 by vpluchar          #+#    #+#             */
-/*   Updated: 2016/11/28 23:16:25 by vpluchar         ###   ########.fr       */
+/*   Updated: 2016/11/29 00:05:30 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_lenword(char *s, char c)
+{
+	int		len;
+
+	len = 0;
+	while (*s != c && *s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
 char		**ft_strsplit(char const *s, char c)
 {
-	char		**tmp;
-	size_t		i;
-	size_t		len;
-	size_t		count;
+	int		nbr1;
+	char	**tab;
+	int		i;
 
-	i = 0;
-	count = ft_countwords(s, c);
-	if (!(tmp = (char **)malloc(sizeof(char *) * (count + 1))))
+	if (!s)
 		return (NULL);
-	count = 0;
-	while (s[i])
+	nbr1 = ft_countwords((char *)s, c);
+	tab = (char **)malloc((nbr1 + 1) * sizeof(char*));
+	i = 0;
+	if (!tab)
+		return (NULL);
+	while (nbr1--)
 	{
-		if (s[i] == c)
-			i++;
-		else
-		{
-			len = 0;
-			while (s[i + len] && s[i + len] != c)
-				len++;
-			tmp[count++] = ft_strsub(s, i, len);
-			i = i + len;
-		}
+		while (*s == c && *s != '\0')
+			s++;
+		tab[i] = ft_strsub((char *)s, 0, ft_lenword((char *)s, c));
+		if (!tab[i])
+			return (0);
+		s = s + ft_lenword((char *)s, c);
+		i++;
 	}
-	tmp[count] = NULL;
-	return (tmp);
+	tab[i] = NULL;
+	return (tab);
 }
