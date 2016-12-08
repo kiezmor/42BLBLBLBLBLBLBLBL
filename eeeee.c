5d3b6075d3f8c7415da282ad9189cc440218c299
Vincent PLUCHARD <vpluchar@e2r11p1.42.fr>
2016-12-08 04:17:57 +0100

    42

---
+++ eeeee.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eeeee.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/08 04:17:47 by vpluchar          #+#    #+#             */
+/*   Updated: 2016/12/08 04:17:50 by vpluchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_repdiez(const char *s, int *nbrp)
+{
+	char	index;
+
+	index = 'A';
+	sauf premiere piece
+	cont = nbrp * 20 + *nbrp;
+	s[cont]
+	while (cont <= (*nbrp + 1) * 20 + *nbrp)
+		if
+
+		else
+			if
+
+			else
+	cont++;
+}
