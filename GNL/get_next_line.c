1c56e019b971a167dc667c0194b3b51905b66f11
Vincent PLUCHARD <vpluchar@e2r12p1.42.fr>
2016-12-09 01:51:30 +0100

    42

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -1,51 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   eeeee.c                                            :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/08 04:17:47 by vpluchar          #+#    #+#             */
-/*   Updated: 2016/12/09 01:31:11 by vpluchar         ###   ########.fr       */
+/*   Created: 2016/12/09 01:46:28 by vpluchar          #+#    #+#             */
+/*   Updated: 2016/12/09 01:47:51 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_repdiez(const char *s, int *nbrp)
-{
-	char	index;
+#include "libft.h"
 
-	index = 'A';
-	sauf premiere piece
-	cont = nbrp * 20 + *nbrp;
-	s[cont]
-	while (cont <= (*nbrp + 1) * 20 + *nbrp)
-		if
-
-		else
-			if
-
-			else
-	cont++;
-}
-
-{
-	char	index;
-
-	index = 'A';
-	
-}
-
-void	ft_putchar(char c);
-
-int	main(void)
-{
-	char	index;
-
-	index = 'A';
-	while (index != 'E')
-	{
-		ft_putchar(index);
-		ft_putchar('\n');
-		index++;
-	}	
-}
+int get_next_line(const int fd, char **line)
+{}
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -1,51 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   eeeee.c                                            :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/08 04:17:47 by vpluchar          #+#    #+#             */
-/*   Updated: 2016/12/09 01:31:11 by vpluchar         ###   ########.fr       */
+/*   Created: 2016/12/09 01:46:52 by vpluchar          #+#    #+#             */
+/*   Updated: 2016/12/09 01:51:18 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_repdiez(const char *s, int *nbrp)
-{
-	char	index;
+#ifndef GET_NEXT_LINE_H
+# define GT_NEXT_LINE_H
 
-	index = 'A';
-	sauf premiere piece
-	cont = nbrp * 20 + *nbrp;
-	s[cont]
-	while (cont <= (*nbrp + 1) * 20 + *nbrp)
-		if
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
 
-		else
-			if
+# define BUFF_SIZE 42
 
-			else
-	cont++;
-}
-
-{
-	char	index;
-
-	index = 'A';
-	
-}
-
-void	ft_putchar(char c);
-
-int	main(void)
-{
-	char	index;
-
-	index = 'A';
-	while (index != 'E')
-	{
-		ft_putchar(index);
-		ft_putchar('\n');
-		index++;
-	}	
-}
+#endif
