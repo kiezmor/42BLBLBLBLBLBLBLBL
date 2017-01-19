8bce9073a9d50c0f31f948e982192c8a8fcbc1d2
Vincent PLUCHARD <vpluchar@e2r12p2.42.fr>
2017-01-19 07:31:01 +0100

    42

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 01:46:28 by vpluchar          #+#    #+#             */
-/*   Updated: 2017/01/18 08:20:55 by vpluchar         ###   ########.fr       */
+/*   Updated: 2017/01/19 07:30:05 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,94 @@ static int	process(const int fd, char *buff, char *s[fd])
 	char		*p;
 	int			i;
 	// char		*t;
+	// int			t;
 	// int			v;
 
-	i = 0;
+	// t = 0;
 	p = ft_strchr(s[fd], '\n');
+	// t = p;
+	// if (ft_strlen(s[fd]) != i && !t)
+	// {
+	// 	t++;
+
+	// }
 	// v = ft_strlen(*s);
-	printf("buff %d\n", *buff);
+	// printf("buff %d\n", *buff);
 	// t = ft_strchr(s[fd], '\0');
-	while (!p && (i = read(fd, buff, BUFF_SIZE)) > 1)
+	// if (buff)
+	// {
+	// 	t = ft_strlen(buff) + 1;
+	// 	printf("kek %d\n", t);
+	// 	buff[t - 1] = '\n';
+	// 	buff[t] = 'o';
+	// }
+	while (!p && (i = read(fd, buff, BUFF_SIZE) > 0))
 	{
-		// if (buff[i] && buff[i + 1] == '\0')
-		// 	buff[i] = '\n';
+		p = ft_strchr(s[fd], '\n');
 		buff[i] = '\0';
 		tmp = s[fd];
 		s[fd] = ft_strjoin(tmp, buff);
-		p = ft_strchr(s[fd], '\n');
 		ft_strdel(&tmp);
+		// if (buff[i] && buff[i + 1] == '\0')// 	buff[i] = '\n';
+		// t = ft_strlen(buff);
+		
+		// else
+		// {
+		// 	// printf("bi %c\n", buff[i]);
+		// buff[i] = '\0';
+		// }
+		
+		// printf("buf %s\n", buff);
+		// if (i < BUFF_SIZE && i != 0 && buff[i] != '\n')
+		// {
+		// 	buff[i] = '\n';
+		// 	buff[i + 1] = '\0';
+		// 	// tmp = s[fd];
+		// 	// printf("buff%s\n", buff);
+		// 	// s[fd] = ft_strjoin(tmp, buff);
+		// 	// ft_strdel(&tmp);
+		// 	// return (1);
+		// }
+		// printf("iwh %d\n", i);
+		// if (buff[i] == '\0' && buff[i] != '\n')
+		// {
+		// 	buff[i] = '\n';
+		// 	// printf("kek %d\n", t);
+		// printf("k%ct\n", buff[i]);
+		// }
 	}
+	// p = ft_strchr(s[fd], '\n');
+	// if (buff[i] != '\0' && buff[i] == '\n' && i > 1)
+	// 	buff[i] = '\0';
+	// else
+	// 	buff[i] = '\n';
+	// if (i > 1 && buff[i] != '\n')
+	// {
+	// 	buff[i] = '\n';
+	// 	p = ft_strchr(s[fd], '\n');
+	// 	i = read(fd, buff, BUFF_SIZE);
+	// }
+	// printf("i %d\n", i);
+	// printf("p %s\n", p);
+	// printf("t %s\n", t);
+	// if (t != 0)
+	// 	return (1);
 	if (i == -1)
 		return (-1);
 	if (i == 0 && !p)
 		return (0);
 	return (1);
 }
+
+// int			check(const int fd, char *buff, char *s[fd])
+// {
+// 	int		i;
+// 	while ((i = read(fd, buff, BUFF_SIZE)) > 1)
+// 	{
+// 		if (i > 1 && buff[i] != '\n')
+// 			buff[i] == '\n';
+// 	}
+// }
 
 int			get_next_line(const int fd, char **line)
 {
