27ca7f0753f77489ae104266ce28b511c168741a
Vincent PLUCHARD <vpluchar@e1r6p14.42.fr>
2017-02-07 04:40:52 +0100

    42

--- GNL/main.c
+++ GNL/main.c
@@ -6,7 +6,7 @@
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 16:58:14 by vpluchar          #+#    #+#             */
-/*   Updated: 2017/01/21 05:46:41 by vpluchar         ###   ########.fr       */
+/*   Updated: 2017/02/07 01:21:02 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,8 @@ int	main(int ac, char **av)
 		fd = open(av[1], O_RDONLY);
 	else
 		fd = 0;
-	// printf("FD %d\n", fd);
 	while ((i = get_next_line(fd, &line)) > 0)
 	{
-		// printf("l |%s|\n", line);
-		// printf("mi|%d|\n", i);
 		printf("%s\n", line);
 	}
 	return (0);
