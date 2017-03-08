bd1ce552cc75ba140f5adcde43336ea2d3acfc6a
Vincent PLUCHARD <vpluchar@e2r9p20.42.fr>
2017-03-08 08:34:20 +0100

    42

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 05:21:07 by vpluchar          #+#    #+#             */
-/*   Updated: 2017/03/08 07:28:11 by vpluchar         ###   ########.fr       */
+/*   Updated: 2017/03/08 08:32:04 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,47 @@
 void		ft_printf(const char * restrict fmt, ...)
 {
 	va_list ap, ap2;
+	int		i;
 
+	i = 0;
 	va_start(ap, fmt);
 	va_copy(ap2, ap);
-		printf("fmt|%s|\n", fmt);
-		if (strcmp(fmt, "%s"))
-			printf("%s", va_arg(ap, char *));
-		else (strcmp(fmt, "%c"));
-			printf("%c", va_arg(ap, int));
+	printf("fmt start|%s|\n", fmt);
+	while (*fmt)
+	{
+		printf("fmt 1|%s|\n", fmt);
+		while (*fmt != '%' && *fmt)
+			++fmt;
+		printf("fmt 2|%s|\n", fmt);
+		if (*fmt == '%')
+		{
+			++fmt;
+			printf("fmt 3|%s|\n", fmt);
+			while (*fmt != '%' && *fmt)
+			{
+				++fmt;
+				++i;
+			}
+			printf("i |%d|\n", i);
+			if (i == 2)
+			{
+				printf("fmt 4|%s|\n", fmt);
+				printf("|%s|\n", "stock2char()");
+				++fmt;
+			}
+			else
+			{
+				printf("fmt 5|%s|\n", fmt);
+				printf("|%s|\n", "stock1char()");
+				++fmt;
+			}
+		}
+	}
+
+		// if (strcmp(fmt, "%s"))
+		// 	printf("%s", va_arg(ap, char *));
+		// else (strcmp(fmt, "%c"));
+		// 	printf("%c", va_arg(ap, int));
 	va_end(ap);
 	va_end(ap2);
 }
@@ -36,5 +69,5 @@ void		ft_printf(const char * restrict fmt, ...)
 int	main()
 {
 	printf("|%s%c|\n", "te", 'c');
-	ft_printf("%s%c", "te", 'c');
+	ft_printf("%s%c%ab", "te", 'c');
 }
