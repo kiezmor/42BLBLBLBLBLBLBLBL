b828255462ced1eabcbe1ba1f048adbd71c29eac
Vincent PLUCHARD <vpluchar@e2r3p1.42.fr>
2017-03-19 08:28:21 +0100

    wip printf

--- printf/age.c
+++ printf/age.c
@@ -1,37 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   age.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpluchar <vpluchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/16 05:21:07 by vpluchar          #+#    #+#             */
-/*   Updated: 2017/03/15 02:05:09 by vpluchar         ###   ########.fr       */
+/*   Created: 2017/03/15 02:11:43 by vpluchar          #+#    #+#             */
+/*   Updated: 2017/03/15 02:41:55 by vpluchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/printf.h"
+#include <stdio.h>
 
-void		ft_printf(const char * restrict fmt, ...)
+int		main()
 {
-	va_list ap, ap2;
-	int		i;
-	char	*tmp;
-
-	i = 0;
-	va_start(ap, fmt);
-	va_copy(ap2, ap);
-	printf("fmt start|%s|\n", fmt);
-	while (*fmt)
-	{
-		
-	}
-	va_end(ap);
-	va_end(ap2);
-}
-
-int	main()
-{
-	printf("|%s%c|\n", "te", 'c');
-	ft_printf("%s%c%ab", "te", 'c');
+	int		i = 13;
+	printf("tu es %s!\n", (i > 17) ? "majeur" : "mineur");
 }
